3ad471833d1890c10752b5181474272e0f7a1f8e
Lenddy <leme2003@gmail.com>
2026-07-24 16:56:22 -0400

    update: updating carousel on the products/ services not (done yet )

--- Client/src/utilities/products-services.js
+++ Client/src/utilities/products-services.js
@@ -4,10 +4,16 @@ export const products = [
 	{
 		name: "Mailboxes",
 		description: "At Webster Locksmiths, we are experts on all types of mailbox systems. We supply and perform custom installations on thousands of different mailbox systems.For replacements on existing systems, we stock and install all vertical, horizontal, or cluster box units. For new installations on new buildings, The Postal Code requires the Auth Florence 4C and we stock and install those systems as well.",
+
 		links: [
-			{ title: "Auth Florence", link: "http://localhost:5173/", img: [{ src: blankkeys, alt: "image-1" }] },
-			{ title: "Bommer", link: "http://localhost:5173/", img: [{ src: blankkeys, alt: "image-2" }] },
-			{ title: "Salsbury Industries", link: "http://localhost:5173/", img: [{ src: blankkeys, alt: "image-3" }] },
+			// { title: "Auth Florence", link: "http://localhost:5173/", img: [{ src: blankkeys, alt: "image-1" }] },
+			{ title: "Auth Florence", link: "http://localhost:5173/", img: [{ src: "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0", alt: "image-1" }] },
+
+			// { title: "Bommer", link: "http://localhost:5173/", img: [{ src: blankkeys, alt: "image-2" }] },
+			{ title: "Bommer", link: "http://localhost:5173/", img: [{ src: "https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0", alt: "image-2" }] },
+
+			// { title: "Salsbury Industries", link: "http://localhost:5173/", img: [{ src: blankkeys, alt: "image-3" }] },
+			{ title: "Salsbury Industries", link: "http://localhost:5173/", img: [{ src: "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0", alt: "image-3" }] },
 		],
 	},
 	{
